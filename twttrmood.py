6c51e920f3ada1b0f479a5ef3bbff3a1f377d748
Rehema Wachira <Rehema@Rehemas-MacBook-Pro.local>
2016-08-26 00:12:04 +0300

    (Feature) Added emoji and figlet formatting to welcome message.
    This file now contains only welcome message and calls all other functions.
    
    ref: #11

--- twttrmood.py
+++ twttrmood.py
@@ -1,27 +1,31 @@
-#import clint
-from OAuth import *
+import emoji
+from colorama import init
+from termcolor import cprint
+from pyfiglet import figlet_format
 from data_processing import *
+from progress_bar import progress_bar
+
+'''
+Application entry point. 
+'''
 
 tweets_list = []
 
 print '*'*50 
-print ":] :/ :[ twttrmood :] :/ :[" #create blinking faces
-print '*'*50 
+print (emoji.emojize(':laughing: :cry: :smiling_imp: :dizzy_face:', use_aliases=True))
+cprint(figlet_format('twttrmood', font='ogre'),
+           'green', attrs=['bold'])
+print (emoji.emojize(':joy: :rage: :yum: :cupid:', use_aliases=True))
+print '*'*50
 handle = raw_input("Enter twitter handle: ")
 
-def get_user_details(handle):  
-	# into message
-	user = tweets.get_user(handle)
-	print "%s has %s followers!" %(user.screen_name, user.followers_count) 
-	print "Here's a list of %s's favourite words:" %(handle)
-
+'''
+All functions are called from this point.
+'''
 get_user_details(handle)
+progress_bar()
 tweets_list = api_results(get_user_tweets(handle))
 list_of_words = tweets_to_words(tweets_list) 
 words_without_stopwords = remove_stop_words(list_of_words)
 wordnum = frequently_used_words(words_without_stopwords)
 sorted_list = sorted_output(wordnum)
-
-
-		
-
