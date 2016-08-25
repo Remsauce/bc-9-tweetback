422168b4779afb0f6a94c291ed71bf58d382f4ed
Rehema Wachira <Rehema@Rehemas-MacBook-Pro.local>
2016-08-26 00:16:28 +0300

    (Feature) Added progress bar

---
+++ progress_bar.py
@@ -0,0 +1,15 @@
+import time, sys
+import progressbar 
+
+def progress_bar():
+
+	bar = progressbar.ProgressBar(widgets=[
+    	'Fetching Tweets:  ',
+    	progressbar.Bar(),
+    	' (', progressbar.Timer(), ') ',
+	])  
+	for i in bar(range(50)):
+		time.sleep(0.5)
+
+
+	
