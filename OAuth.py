b3689e3149fabe3ff9f4972cf1de3b32e82e0566
Rehema Wachira <Rehema@Rehemas-MacBook-Pro.local>
2016-08-26 00:17:50 +0300

    (Feature) Updated docString

--- OAuth.py
+++ OAuth.py
@@ -1,6 +1,7 @@
-import tweepy, os
+import tweepy
 import json as simplejson
 from config import *
+import progressbar
 
 '''
 Use Tweepy to manage authentication between 
@@ -11,11 +12,10 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 # create a Twitter API object called tweets which establishes the connection.
-tweets = tweepy.API(auth)
-  
+tweets = tweepy.API(auth) 
+
 def get_user_tweets(handle): 
 	# fetch timeline tweets from a specified user's handle
-	user_tweets = tweets.user_timeline(screen_name = handle)
+	user_tweets = tweets.user_timeline(screen_name = handle, count = 200)
 	return user_tweets
-
 
